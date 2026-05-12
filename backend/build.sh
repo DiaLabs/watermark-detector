5e648127de4ca2682794d549fea66cc24e6a7c89
akanupam <aditanupam.gmail.com>
2026-05-12 16:05:53 +0530

    Update backend dependencies to support Python 3.14

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-pip install --upgrade pip
-pip install --prefer-binary -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --prefer-binary --only-binary :all: -r requirements.txt || pip install --prefer-binary -r requirements.txt
+
